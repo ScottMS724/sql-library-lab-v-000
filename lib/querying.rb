f4533968f5d17d1086809ce6cb452665a3c2f585
Scott Silverman <scottms724@gmail.com>
2019-04-03 17:25:46 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,8 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  SELECT series.title, characters.species, COUNT(chracters.species)
+  SELECT series.title, characters.species, COUNT(characters.species)
+  FROM 
 end
 
 def select_character_names_and_number_of_books_they_are_in
