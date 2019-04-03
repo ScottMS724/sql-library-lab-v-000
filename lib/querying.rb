495615fd5111d8e044ca5654c56e3ead11310298
Scott Silverman <scottms724@gmail.com>
2019-04-03 17:22:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ end
 def select_name_and_series_subgenres_of_authors
   SELECT authors.name, subgenres.name 
   FROM series 
-  INNER JOIN authors ON series.author_id = 
+  INNER JOIN authors ON series.author_id = authors.id 
 end
 
 def select_series_title_with_most_human_characters
