96e1a1660d51266a1dd8f8613a368d328b3f3ae0
Scott Silverman <scottms724@gmail.com>
2019-04-03 17:27:30 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,7 @@ end
 
 def select_series_title_with_most_human_characters
   SELECT series.title, characters.species, COUNT(characters.species)
-  FROM 
+  FROM series
 end
 
 def select_character_names_and_number_of_books_they_are_in
